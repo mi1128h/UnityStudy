f261abe95ea98a6dcfa25482d6d18bb7da95033d
mi1128h <70762557+mi1128h@users.noreply.github.com>
2022-05-27 12:00:54 +0900

    13주차 - 3
    2D player jump

--- 13주차/Assets/Player.cs
+++ 13주차/Assets/Player.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float jumpHeight = 5f;
     public float speed;
+    public Transform footPosition;
+    public LayerMask groundLayer;
+    
     private Rigidbody2D _rigidbody;
-    private Vector2 _playerInput;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
+    
+    private Vector2 _playerInput;
+    private bool _isGround;
     
     // Start is called before the first frame update
     void Start()
@@ -25,11 +31,19 @@ public class Player : MonoBehaviour
 
         if (_rigidbody.velocity.x > 0) _spriteRenderer.flipX = false;
         else if (_rigidbody.velocity.x < 0) _spriteRenderer.flipX = true;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpHeight);
+        }
     }
 
     void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_playerInput.x * Time.fixedDeltaTime * speed, _rigidbody.velocity.y);
         _animator.SetFloat("move_speed", Mathf.Abs(_rigidbody.velocity.x));
+
+        var hit2D = Physics2D.BoxCast(footPosition.position, new Vector2(0.1f, 0.05f), 0f, Vector2.down, 0, groundLayer);
+        _isGround = hit2D.collider != null;
     }
 }
